c876eb1295854f8bc58e8755d0aadae55083bab4
Ajstros <ajstros@gmail.com>
2023-05-09 13:05:37 -0500

    Fix documentation path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import sys
 import os
 
 # Add image432 to path for autodoc use
-sys.path.insert(0, os.path.abspath("../../"))
+sys.path.insert(0, os.path.abspath("../../src"))
 
 # -- Project information -----------------------------------------------------
 
